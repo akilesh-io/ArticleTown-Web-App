3eca01b3009b6cc48706aeeaf1089ed236988576
Akilesh <53332156+2112akilesh@users.noreply.github.com>
2021-04-14 10:42:30 +0530

    Login work are done

--- src/app/tools/authenticator/authenticator.component.ts
+++ src/app/tools/authenticator/authenticator.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FirebaseTSAuth } from 'firebasets/firebasetsAuth/firebaseTSAuth';  //Import the FirebaseTSAuth class
 
 @Component({
   selector: 'app-authenticator',
@@ -7,37 +8,125 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AuthenticatorComponent implements OnInit {
 
-  
-  constructor() {  }
+  //Instantiate an FirebaseTSAuth object.
+  private auth: FirebaseTSAuth;
+  constructor() {
+    this.auth = new FirebaseTSAuth();
+  }
   ngOnInit(): void {
   }
-  state = AuthenticatorCompState.LOGIN;
+
+  //Create Account Firebase works *reference*: (https://codeible.com/view/course/inGXZQSL9Xp7X7iSzuyZ;title=Firebase%20Authentication:%20Creating%20Users)
+  //-----Create-----
+  onRegisterClick(
+    registerEmail: HTMLInputElement,
+    registerPassword: HTMLInputElement,
+    registerConfirmPassword: HTMLInputElement
+  ) {
+    let email = registerEmail.value;
+    let password = registerPassword.value;
+    let confirmPassword = registerConfirmPassword.value;
+    if (
+      this.isNotEmpty(email) &&
+      this.isNotEmpty(password) &&
+      this.isNotEmpty(confirmPassword) &&
+      this.isAMatch(password, confirmPassword)
+    ) {
+      this.auth.createAccountWith(
+        {
+          email: email,
+          password: password,
+          onComplete: (uc) => {
+            alert("Account Created");
+            registerEmail.value = "";
+            registerPassword.value = "";
+            registerConfirmPassword.value + "";
+          },
+          onFail: (err) => {
+            alert("Failed to Sign in...");
+          }
+        }
+      );
+    }
+  }
+
+  //-----Login-----
+  onLogin(
+    loginEmail: HTMLInputElement,
+    loginPassword: HTMLInputElement
+  ) {
+    let email = loginEmail.value;
+    let password = loginPassword.value;
+
+    if (this.isNotEmpty(email) && this.isNotEmpty(password)) {
+      this.auth.signInWith(
+        {
+          email: email,
+          password: password,
+          onComplete: (uc) => {
+            alert("Login Successful");
+          },
+          onFail: (err) => {
+            alert(err);
+          }
+
+        },
+      );
+    }
+
+  }
+
+  //-----Reset Password-----
+  onResetPassword(resetPassword:HTMLInputElement){
+    let email = resetPassword.value;
+    if(this.isNotEmpty(email)){
+      this.auth.sendPasswordResetEmail(
+        {
+          email:email,
+          onComplete: (uc) => {
+            alert(`Reset password has been sent to ${email}`);
+          }
+        }
+      );
+    }
+  }
+
+  //Checking if the mail or password is correctly filled
+  isNotEmpty(text: string) {
+    return text != null && text.length > 0;
+  }
+
+  isAMatch(text: string, ComparedWith: string) {
+    return text == ComparedWith;
+  }
 
   //Navigate between login reset create using ng-if directive
-  onLoginClick(){
+  state = AuthenticatorCompState.LOGIN;
+
+  onLoginClick() {
     this.state = AuthenticatorCompState.LOGIN;
   }
-  onCreateAccountClick(){
+  onCreateAccountClick() {
     this.state = AuthenticatorCompState.REGISTER;
   }
-  onForgotPasswordClick(){
+  onForgotPasswordClick() {
     this.state = AuthenticatorCompState.FORGOT_PASSWORD;
   }
 
 
 
-  isLoginState(){
+  isLoginState() {
     return this.state == AuthenticatorCompState.LOGIN;
   }
-  isRegisterState(){
+  isRegisterState() {
     return this.state == AuthenticatorCompState.REGISTER;
   }
-  isForgotPassword(){
+  isForgotPassword() {
     return this.state == AuthenticatorCompState.FORGOT_PASSWORD;
   }
 }
 
-export enum AuthenticatorCompState{
+export enum AuthenticatorCompState {
   LOGIN,
   REGISTER,
   FORGOT_PASSWORD
